caa8c17e5913762485da760ce36e709254550f86
Simon Landis <simon.landis@gmail.com>
2026-03-19 12:09:16 -0400

    Initial Vite commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-white">My Little Portfolio</h1>
+    </div>
+  )
+}
